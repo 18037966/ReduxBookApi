e66e6d3738641807ca8b11f0021c37b78f08045d
Zubaiyer Karim Labib <zbaiyer.labib@gmail.com>
2017-10-08 00:23:43 +1100

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function(){
+	return[
+		{ title: 'Javascript', pages: 101},
+		{ title: 'Harry potter', pages: 81},
+        { title: 'JEloquent', pages: 34},
+ 		{ title: 'The dark tower', pages:12}
+       
+	]
+}
